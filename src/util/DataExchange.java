a140841ccbd21c3aa0f72ec7002e514b8b51b127
Imran R <67403229+space-ninja-x@users.noreply.github.com>
2021-12-22 09:12:47 +0530

    Add files via upload

---
+++ src/util/DataExchange.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2021 by Imran R.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package util;
+
+import model.Bell;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is used to read and write data locally on the computer
+ */
+public class DataExchange
+{
+    public static void writeToFile(String pathToFile, List<Bell> list) throws Exception
+    {
+        FileOutputStream fileOutputStream = new FileOutputStream(pathToFile);
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        for(Bell b : list)
+        {
+            objectOutputStream.writeObject(b.getBellID());
+            objectOutputStream.writeObject(b.getStartTime());
+            objectOutputStream.writeObject(b.getEndTime());
+            objectOutputStream.writeObject(b.getPeriod());
+            objectOutputStream.writeObject(b.getPeriodInSeconds());
+            objectOutputStream.writeObject(b.getRepeat());
+            objectOutputStream.writeObject(b.getDescription());
+            objectOutputStream.writeObject(b.getPathToMediaFile());
+        }
+
+        objectOutputStream.close();
+        fileOutputStream.close();
+    }
+
+
+    public static List<Bell> readFromFile(String pathToFile) throws Exception
+    {
+        List<Bell> list = new ArrayList<>() ;
+
+        try
+        {
+            FileInputStream fileInputStream = new FileInputStream(pathToFile) ;
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream) ;
+
+            while (true)
+            {
+                Bell bell = new Bell() ;
+
+                bell.setBellID((String) objectInputStream.readObject()) ;
+                bell.setStartTime((String) objectInputStream.readObject()) ;
+                bell.setEndTime((String) objectInputStream.readObject()) ;
+                bell.setPeriod((String) objectInputStream.readObject()) ;
+                bell.setPeriodInSeconds((int) objectInputStream.readObject()) ;
+                bell.setRepeat((String) objectInputStream.readObject()) ;
+                bell.setDescription((String) objectInputStream.readObject()) ;
+                bell.setPathToMediaFile((String) objectInputStream.readObject()) ;
+
+                list.add(bell) ;
+            }
+        }
+        catch (EOFException eofException)
+        {
+            System.out.println("\nDataExchange.readFromFile: End of file reached!\n") ;
+        }
+        catch (ClassNotFoundException ce)
+        {
+            ce.printStackTrace();
+        }
+
+        return list ;
+    }
+}
---
+++ src/views/AddBellViewController.java
@@ -0,0 +1,407 @@
+/*
+ * Copyright (c) 2021 by Imran R.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package views;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import model.BellCollection;
+import util.IDGenerator;
+import values.Repeat;
+
+
+public class AddBellViewController implements Initializable
+{
+    public AnchorPane root ;
+    public VBox mainVBox ;
+    public Label header, warningLbl ;
+    public Button addBtn, searchBtn ;
+    public TextField periodField, descriptionField, audioFileField;
+    public ChoiceBox<String> startChoiceBox, endChoiceBox, periodChoiceBox ;
+    public ComboBox<String> startHr, startMin, endHr, endMin, repeatBox;
+    public CheckBox checkBox ;
+
+    private final MessageBox messageBox = new MessageBox() ;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        ImageView imageView = new ImageView(new Image(Objects.requireNonNull(this.getClass().getResource("/resources/images/search_icon.png")).toString()));
+        imageView.setFitWidth(15.0) ;
+        imageView.setFitHeight(15.0) ;
+
+        searchBtn.setGraphic(imageView) ;
+
+        initialiseBoxes() ;
+
+        checkBox.setOnAction(e ->
+        {
+            if(checkBox.isSelected())
+            {
+                endHr.setDisable(true) ;
+                endMin.setDisable(true) ;
+                endChoiceBox.setDisable(true) ;
+                periodField.setDisable(true) ;
+                periodChoiceBox.setDisable(true) ;
+            }
+            else
+            {
+                endHr.setDisable(false) ;
+                endMin.setDisable(false) ;
+                endChoiceBox.setDisable(false) ;
+                periodField.setDisable(false) ;
+                periodChoiceBox.setDisable(false) ;
+            }
+        }) ;
+
+        root.setId("root_style") ;
+        header.setId("header_style") ;
+        warningLbl.setId("warning_lbl_style");
+    }
+
+
+    /**
+     * Method to initialise the ChoiceBoxes and ComboBoxes in the application to allow
+     * user interaction.
+     */
+    private void initialiseBoxes()
+    {
+        ObservableList<String> list1 = FXCollections.observableArrayList() ;
+        list1.addAll("AM", "PM") ;
+
+        ObservableList<String> list2 = FXCollections.observableArrayList() ;
+        list2.addAll("Minutes", "Seconds") ;
+
+        ObservableList<String> listOfHours = FXCollections.observableArrayList() ;
+        listOfHours.addAll("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12") ;
+
+        ObservableList<String> listOfRepeatValues = FXCollections.observableArrayList() ;
+        listOfRepeatValues.addAll(Repeat.NONE, Repeat.EVERY_WEEK, Repeat.EVERY_MONDAY, Repeat.EVERY_TUESDAY, Repeat.EVERY_WEDNESDAY, Repeat.EVERY_THURSDAY, Repeat.EVERY_FRIDAY) ;
+
+        ObservableList<String> listOfNum = FXCollections.observableArrayList() ;
+        for(int counter = 0 ; counter < 60 ; counter++)
+        {
+            String temp ;
+
+            if(counter < 10)
+                temp = "0" + counter;
+            else
+                temp = Integer.toString(counter) ;
+
+            listOfNum.add(temp) ;
+        }
+
+        // set the items
+        startHr.setItems(listOfHours) ;
+        startMin.setItems(listOfNum) ;
+        endHr.setItems(listOfHours) ;
+        endMin.setItems(listOfNum) ;
+        startChoiceBox.setItems(list1);
+        endChoiceBox.setItems(list1);
+        periodChoiceBox.setItems(list2);
+        repeatBox.setItems(listOfRepeatValues) ;
+
+        startHr.setVisibleRowCount(10) ;
+        startMin.setVisibleRowCount(10) ;
+        endHr.setVisibleRowCount(10) ;
+        endMin.setVisibleRowCount(10) ;
+        repeatBox.setVisibleRowCount(3) ;
+
+        startHr.setValue("Hr") ;
+        startMin.setValue("Min") ;
+        endHr.setValue("Hr") ;
+        endMin.setValue("Min") ;
+        startChoiceBox.setValue("AM") ;
+        endChoiceBox.setValue("AM") ;
+        periodChoiceBox.setValue("Minutes") ;
+        repeatBox.setValue(Repeat.NONE);
+    }
+
+
+    /**
+     * Method to get the starting time set
+     * @return The starting time in the 24-hour format HH:MM
+     */
+    private String getStartTime()
+    {
+        String startTime ;
+
+        // set the starting time
+        if(startChoiceBox.getValue().equals("PM"))
+        {
+            int h = Integer.parseInt(startHr.getValue()) ;
+
+            h+=12 ;
+
+            startTime = h + ":" + startMin.getValue() ;
+        }
+        else
+        {
+            startTime = startHr.getValue() + ":" + startMin.getValue() ;
+        }
+
+        return  startTime ;
+    }
+
+
+    /**
+     * Method to get the end time set
+     * @return The end time in the format HH:MM
+     */
+    private String getEndTime()
+    {
+        String endTime ;
+
+        if(endChoiceBox.getValue().equals("PM"))
+        {
+            int h = Integer.parseInt(endHr.getValue()) ;
+
+            h+=12 ;
+
+            endTime = h + ":" + endMin.getValue() ;
+        }
+        else
+        {
+            endTime = endHr.getValue() + ":" + endMin.getValue() ;
+        }
+
+        return  endTime ;
+    }
+
+
+    /**
+     * @param hours the hours in the 24-hour format HH:MM to be converted
+     * @return the hours in seconds
+     */
+    private Integer hoursToSeconds(String hours)
+    {
+        int inSeconds = 0 ;
+
+        inSeconds = inSeconds + (Integer.parseInt(hours.substring(0, 2)) * 60 * 60) ;       // convert the hour part
+        inSeconds = inSeconds + (Integer.parseInt(hours.substring(3, 5)) * 60) ;            // convert the minute part
+
+        return inSeconds ;
+    }
+
+
+    /**
+     * Checks if the start time is valid given an ending time
+     * @return true if valid else false
+     */
+    private Boolean validateTimes()
+    {
+        boolean valid = true ;
+
+        int startTimeInSeconds = hoursToSeconds(getStartTime()) ;
+        int endTimeInSeconds = hoursToSeconds(getEndTime()) ;
+
+        if(endTimeInSeconds <= startTimeInSeconds)   valid = false ;
+
+
+
+
+        return valid ;
+    }
+
+
+    /**
+     * Method to start the timer or cancel it depending on the current state
+     */
+    public void add()
+    {
+        String description = "-" ;
+        String val = Repeat.NONE;
+
+        switch (repeatBox.getValue())
+        {
+            case "EVERY WEEK":
+                val = Repeat.EVERY_WEEK ;
+                break ;
+            case "EVERY MONDAY":
+                val = Repeat.EVERY_MONDAY ;
+                break ;
+            case "EVERY TUESDAY":
+                val = Repeat.EVERY_TUESDAY ;
+                break ;
+            case "EVERY WEDNESDAY":
+                val = Repeat.EVERY_WEDNESDAY ;
+                break ;
+            case "EVERY THURSDAY":
+                val = Repeat.EVERY_THURSDAY ;
+                break ;
+            case "EVERY FRIDAY":
+                val = Repeat.EVERY_FRIDAY ;
+                break ;
+        }
+
+        if(!this.descriptionField.getText().isBlank())
+            description = descriptionField.getText() ;
+
+
+        if(!checkBox.isSelected())
+        {
+            if (startHr.getValue().equals("Hr") || startMin.getValue().equals("Min") || endHr.getValue().equals("Hr") || endMin.getValue().equals("Min"))
+            {
+                this.messageBox.show("You must assign a start and an end time!", "ERROR");
+            }
+            else if(audioFileField.getText().isBlank())
+            {
+                this.messageBox.show("Please select an audio file.", "ERROR") ;
+            }
+            else
+            {
+                int period = 0;
+
+                if(!this.periodField.getText().isBlank())
+                {
+                    period = Integer.parseInt(this.periodField.getText());
+
+                    if(periodChoiceBox.getValue().equals("Minutes"))    period = period * 60;
+                }
+
+                try
+                {
+                    if(validateTimes())
+                    {
+                        if(validateMediaFile())
+                        {
+                            BellCollection.getInstance().addNewBell(generateBellID(), getStartTime(), getEndTime(), period, val, this.audioFileField.getText(), description) ;
+
+                            ((Stage) this.root.getScene().getWindow()).close();
+                        }
+                        else
+                        {
+                            messageBox.show("Only .mp3 and .wav files are allowed! Select another audio file.", "ERROR") ;
+                        }
+                    }
+                    else
+                    {
+                        messageBox.show("The times entered are not valid.", "ERROR");
+                    }
+                }
+                catch (Exception e)
+                {
+                    messageBox.show("Oops! Something went wrong. Please check the details you've entered and try again.", "ERROR");
+
+                    e.printStackTrace();
+                }
+            }
+        }
+        else
+        {
+            if(startHr.getValue().equals("Hr") || startMin.getValue().equals("Min"))
+            {
+                messageBox.show("You must assign a start time!", "ERROR") ;
+            }
+            else
+            {
+                if(validateMediaFile())
+                {
+                    try
+                    {
+                        BellCollection.getInstance().addNewBell(generateBellID(), getStartTime(), "", 0, val, this.audioFileField.getText(), description);
+
+                        ((Stage) this.root.getScene().getWindow()).close();
+                    }
+                    catch (Exception e)
+                    {
+                        messageBox.show("Oops! Something went wrong. Please check the details you've entered and try again.", "ERROR");
+
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    messageBox.show("Only .mp3 and .wav files are allowed! Select another audio file.", "ERROR") ;
+                }
+            }
+        }
+    }
+
+
+    public void openFileChooser()
+    {
+        FileChooser fileChooser = new FileChooser() ;
+        fileChooser.setTitle("Browse media file") ;
+
+        File mediaFile = fileChooser.showOpenDialog(this.root.getScene().getWindow()) ;
+        String filePath = "" ;
+
+        if(mediaFile != null)   filePath = mediaFile.getPath() ;
+
+        this.audioFileField.setText(filePath) ;
+    }
+
+
+    /**
+     * Checks the file extension of the picked audio file and validates it
+     * @return true if valid (.wav or .mp3), otherwise false
+     */
+    private boolean validateMediaFile()
+    {
+        boolean valid = false ;
+        String filePath = this.audioFileField.getText() ;
+        String fileExtension = filePath.substring(filePath.lastIndexOf(".")) ;
+
+        if(fileExtension.equalsIgnoreCase(".mp3") || fileExtension.equalsIgnoreCase(".wav"))
+        {
+            valid = true ;
+        }
+
+        return valid ;
+    }
+
+
+    private String generateBellID()
+    {
+        String id ;
+        int counter = 0 ;
+        boolean found ;
+
+        do
+        {
+            id = IDGenerator.generateID(2, 4) ;
+            found = false ;
+
+            while(!found && counter < BellCollection.getInstance().getListOfBells().size())
+            {
+                if(BellCollection.getInstance().getListOfBells().get(counter).getBellID().equals(id))
+                    found = true;
+
+                counter++;
+            }
+        }
+        while(found) ;
+
+        return id ;
+    }
+}
